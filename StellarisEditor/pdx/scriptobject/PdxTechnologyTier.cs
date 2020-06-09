4f0f21f9d2cfee657785606d2489aa18e0f1c433
利维坦上的小海星 <59285120@qq.com>
2020-06-09 19:24:44 +0800

    add technology tier class

---
+++ StellarisEditor/pdx/scriptobject/PdxTechnologyTier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarisEditor.pdx.scriptobject
+{
+    public class PdxTechnologyTier : PdxObject
+    {
+        public new event PropertyChangedEventHandler PropertyChanged;
+    }
+}
