af1c6ca57d7c141d96459ea6d866870019707f8e
利维坦上的小海星 <59285120@qq.com>
2020-06-10 09:53:29 +0800

    Rename the folder name CodeEngine to ScriptEngine, and reset the CharStream file namespace.

--- StellarisEditor/ScriptEngine/CharStream.cs
+++ StellarisEditor/ScriptEngine/CharStream.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StellarisEditor.CodeEngine
+namespace StellarisEditor.ScriptEngine
 {
     /// <summary>
     /// 字符流
