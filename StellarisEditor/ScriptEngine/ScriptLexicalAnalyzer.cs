d40dd7e299cb557f16d19f1c12721daa073d5df9
利维坦上的小海星 <59285120@qq.com>
2020-06-10 10:05:02 +0800

    Create new file ScriptLexicalAnalyzer

---
+++ StellarisEditor/ScriptEngine/ScriptLexicalAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarisEditor.ScriptEngine
+{
+    /// <summary>
+    /// <para>脚本词法分析器</para>
+    /// <para>本类用来扫描脚本代码，生成代码单元</para>
+    /// </summary>
+    public class ScriptLexicalAnalyzer
+    {
+
+    }
+}
