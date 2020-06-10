f16d347f9b7e93f2d6d9fde5179559e6da173fb0
利维坦上的小海星 <59285120@qq.com>
2020-06-10 13:35:45 +0800

    Add ScriptParser class

---
+++ StellarisEditor/ScriptEngine/ScriptParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarisEditor.ScriptEngine
+{
+    /// <summary>
+    /// 脚本解析器
+    /// </summary>
+    public class ScriptParser
+    {
+        /// <summary>
+        /// 内置词法分析器
+        /// </summary>
+        private ScriptLexicalAnalyzer Lexical { get; set; }
+
+        /// <summary>
+        /// 根据指定得词法分析器创建解析器
+        /// </summary>
+        /// <param name="lex"></param>
+        public ScriptParser(ScriptLexicalAnalyzer lex)
+        {
+            Lexical = lex;
+        }
+    }
+}
