c48bfcda0a781a51332094600ded284af89c0144
XSkiper <xskiper@163.com>
2020-06-12 15:45:53 +0800

    议程编辑器界面

---
+++ StellarisEditor/pdx/scriptobject/PdxAgenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarisEditor.pdx.scriptobject
+{
+    public class PdxAgenda
+    {
+        public String Key;
+        public LinkedList<PdxModifier> Modifier;
+        public PdxWeightModifier WeightModifier;
+    }
+}
