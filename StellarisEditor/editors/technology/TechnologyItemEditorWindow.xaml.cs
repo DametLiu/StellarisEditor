e334e111f6b48b467ff1e27c264f57e76cb10bf8
infinite-create <y246800@qq.com>
2020-06-13 17:40:28 +0800

    1.完成technology条目预览窗口（空壳） 2.完成civic,technology编辑窗口控件之间的逻辑

--- StellarisEditor/editors/technology/TechnologyItemEditorWindow.xaml.cs
+++ StellarisEditor/editors/technology/TechnologyItemEditorWindow.xaml.cs
@@ -23,5 +23,79 @@ namespace StellarisEditor.editors.technology
         {
             InitializeComponent();
         }
+
+        private void Circulation_Click(object sender, RoutedEventArgs e)
+        {
+            if (Circulation.IsChecked.Value)
+            {
+                Cost_Pre_Level.IsEnabled = true;
+            }
+            else
+            {
+                Cost_Pre_Level.IsEnabled = false;
+            }
+        }
+
+        private void Ship_Click(object sender, RoutedEventArgs e)
+        {
+            if (Ship.IsChecked.Value)
+            {
+                Ship_title.IsEnabled = true;
+                Ship_desc.IsEnabled = true;
+            }
+            else
+            {
+                Ship_title.IsEnabled = false;
+                Ship_desc.IsEnabled = false;
+            }
+        }
+
+        private void Custom_Click(object sender, RoutedEventArgs e)
+        {
+            if (Custom.IsChecked.Value)
+            {
+                Custom_desc.IsEnabled = true;
+                Custom_title.IsEnabled = true;
+            }
+            else
+            {
+                Custom_desc.IsEnabled = false;
+                Custom_title.IsEnabled = false;
+            }
+        }
+
+        private void Check_Gateway_Click(object sender, RoutedEventArgs e)
+        {
+            if (Check_Gateway.IsChecked.Value)
+            {
+                Gateway.IsEnabled = true;
+            }
+            else
+            {
+                Gateway.IsEnabled = false;
+            }
+        }
+
+        private void Start_Technology_Click(object sender, RoutedEventArgs e)
+        {
+            if (Start_Technology.IsChecked.Value)
+            {
+                Cost.IsEnabled = false;
+                Tier.IsEnabled = false;
+                Ai_Weight.IsEnabled = false;
+                Weight_Modifier.IsEnabled = false;
+                Modifier.IsEnabled = false;
+                Tier.Text = "0";
+            }
+            else
+            {
+                Cost.IsEnabled = true;
+                Tier.IsEnabled = true;
+                Ai_Weight.IsEnabled = true;
+                Weight_Modifier.IsEnabled = true;
+                Modifier.IsEnabled = true;
+                Tier.Text = null;
+            }
+        }
     }
 }
